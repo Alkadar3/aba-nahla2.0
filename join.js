1273d677faa753f9f24c359012aa35c8d505cbe4
Alkadar3 <72749959+Alkadar3@users.noreply.github.com>
2020-10-12 18:32:52 +0300

    Update join.js

--- join.js
+++ join.js
@@ -1,11 +1,11 @@
 const commando = require('omarka.js-commando')
 
-class joincommand extends commando.command
+class join extends commando.command
 {
 constructor(client)
 {super(client,{
 name: 'join',
-group: 'music',
+group: 'join',
 member name: 'join',
 description: 'yyyu'
 });
